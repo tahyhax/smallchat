fa8683959a4b78b6f48c34fd9c06dee1af7fe406
tahyhax <dimon4i4ek@ukr.net>
2020-07-15 23:09:44 +0300

    SM-1 edit  schema structure

--- models/Message.js
+++ models/Message.js
@@ -1,23 +1,26 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const MessageChema = mongoose.Schema({
-  chat: {
-    type: Schema.Types.ObjectId,
-    ref: "Chat",
+const MessageChema = mongoose.Schema(
+  {
+    chat: {
+      type: Schema.Types.ObjectId,
+      ref: "Chat",
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    // time: {
+    //   type: Number,
+    //   required: true,
+    // },
   },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  text: {
-    type: String,
-    required: true,
-  },
-  time: {
-    type: Number,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Message", MessageChema);
