770178f5c9f3f67db749124b4d261296e716b762
tahyhax <dimon4i4ek@ukr.net>
2020-07-17 00:05:53 +0300

    SM-1  add new  method  getUserContacts

--- services/users.js
+++ services/users.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const Chat = require("../models/Chat");
 
 async function createUser(data) {
   try {
@@ -9,9 +10,21 @@ async function createUser(data) {
     return Promise.reject(error);
   }
 }
+
+async function getUserContacts(id) {
+  try {
+    const user = await User.find(
+      { contacts: { $in: [id] } },
+      { email: 1, firstName: 1, lastName: 1 }
+    );
+    return user;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
 async function getUserByEmail(email) {
   try {
-    const user = await User.findOne({ email: email }).populate("contacts");
+    const user = await User.findOne({ email: email });
     return user;
   } catch (error) {
     return Promise.reject(error);
@@ -19,12 +32,35 @@ async function getUserByEmail(email) {
 }
 async function joinUserInChat({ chatId, userId }) {
   try {
-    const user = await User.findByIdAndUpdate(
-      {
-        _id: userId,
-        chats: { $nin: chatId },
-      },
-      { $push: { chats: chatId } }
+    // const user = await User.findByIdAndUpdate(
+    //   {
+    //     _id: userId,
+    //     chats: { $nin: chatId },
+    //   },
+    //   { $push: { chats: chatId } }
+    // );
+    Promise.all([
+      await User.findByIdAndUpdate(
+        {
+          _id: userId,
+          chats: { $nin: chatId },
+        },
+        { $push: { chats: chatId } }
+      ),
+      await asigneUserToChat({ chatId, userId }),
+    ]);
+    return true;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+async function asigneUserToChat({ chatId, userId }) {
+  try {
+    console.log("asigneUserToChat", chatId);
+    await Chat.findByIdAndUpdate(
+      chatId,
+      { $set: { users: userId } },
+      { new: true, useFindAndModify: false }
     );
     return true;
   } catch (error) {
@@ -50,6 +86,7 @@ async function addUserNewMessage({ userId, chatId, messageId }) {
 
 module.exports = {
   createUser,
+  getUserContacts,
   getUserByEmail,
   joinUserInChat,
   addUserNewMessage,
